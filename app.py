f466b037a04b093b4f11f1e0799378c75685f549
Boonyarit Iasma-ard <boonyarit.iamsaard@gmail.com>
2020-06-07 21:42:34 +0700

    Update tables

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, url_for
 import pymysql
 import query
+import datetime
 
 app = Flask(__name__)
 # conn = pymysql.connect("us-cdbr-east-05.cleardb.net", "b02b9837c2f815", "1628b5ed", "heroku_13912b51418014f")
 
 
-conn = pymysql.connect("localhost", "root", "cxbkkeng", "cxbkkstn")
+conn = pymysql.connect("localhost", "root", "cxbkkeng", "cxbkkeng")
 
 
 @app.route("/")
@@ -14,7 +15,7 @@ def dashboard():
     def flight():
         with conn:
             cur = conn.cursor()
-            cur.execute(query.flightSql)
+            cur.execute(query.totalflt)
             total = cur.fetchall()
             return total
 
@@ -47,37 +48,138 @@ def dashboard():
 
     wy = wy()
 
-    def pkgadd():
+    def addclrd():
         with conn:
             cur = conn.cursor()
-            cur.execute(query.pkdadd)
-            totalpkgadd = cur.fetchall()
-            return totalpkgadd
+            cur.execute(query.addclrd)
+            add = cur.fetchone()
+            return add
 
-    pkgadd = pkgadd()
+    totaladdclrd = addclrd()
 
-    def sumadd():
+    def addworked():
         with conn:
             cur = conn.cursor()
-            cur.execute(query.sumadd)
+            cur.execute(query.addworked)
             totaladd = cur.fetchone()
             return totaladd
 
-    sumadd = sumadd()
+    totaladdworked = addworked()
+
+    def pkg():
+        with conn:
+            cur = conn.cursor()
+            cur.execute(query.pkg)
+            totalpkg = cur.fetchone()
+            return totalpkg
+
+    pkg = pkg()
 
     # flight = data.0
     # check = data.1
     # ovn = data.2
     # wy = data.3
     # pkgadd = data.4
-    # sumadd = data.5
-    datas = (flight, check, ovn, wy, pkgadd, sumadd)
-    return render_template("dashboard.html", data=datas)
+    # addclrd = data.5
+    # addworked = data.6
+    # pkg = data.7
+    data = (flight, check, ovn, wy, totaladdclrd, totaladdworked, pkg)
+    return render_template("dashboard.html", title='Dashboard', data=data)
 
 
 @app.route('/create')
 def create():
-    return render_template('create.html')
+    def staff():
+        with conn:
+            cur = conn.cursor()
+            cur.execute(query.staff)
+            namelist = cur.fetchall()
+            return namelist
+
+    name = staff()
+
+    def eic():
+        with conn:
+            cur = conn.cursor()
+            cur.execute(query.eic)
+            recentlyadded = cur.fetchall()
+            return recentlyadded
+
+    eic = eic()
+
+    data = (name, eic)
+
+    return render_template('create.html', title='Add Flight', data=data)
+
+
+@app.route('/insert', methods=['POST'])
+def insert():
+    if request.method == 'POST':
+        # Flight details
+        arrdate = request.form['arrdate']
+        airline = request.form['airline']
+        fltno = request.form['fltno']
+        prefix = request.form['prefix']
+        acreg = request.form['acreg']
+        ata = request.form['ata']
+        atd = request.form['atd']
+        bay = request.form['bay']
+        chk = request.form['chk']
+        # Servicing
+        watersvc = request.form['watersvc']
+        wastesvc = request.form['wastersvc']
+        afac = request.form['afac']
+        gpu = request.form['gpu']
+        asu = request.form['asu']
+        acu = request.form['acu']
+        brk = request.form['brk']
+        cherry = request.form['cherry']
+        platform = request.form['platform']
+        # Work Package & ADDs
+        pkg = request.form['pkg']
+        padd = request.form['padd']
+        sadd = request.form['sadd']
+        add = request.form['add']
+        zadd = request.form['zadd']
+        cadd = request.form['cadd']
+        madd = request.form['madd']
+        worked = request.form['work']
+        # Overnight
+        ovnbay = request.form['ovnbay']
+        tpc = request.form['tpc']
+        waterdrain = request.form['waterdrain']
+        wastedrain = request.form['wastedrain']
+        fueldrain = request.form['fueldrain']
+        stand = request.form['stand']
+        acwsh = request.form['acwsh']
+        # Handling By
+        mech1 = request.form['mech1']
+        mech2 = request.form['mech2']
+        eng = request.form['eic']
+        tda = request.form['tda']
+        # Flight remark
+        fltrmk = request.form['fltrmk']
+        record = request.form['record']
+        print(type(arrdate))
+        with conn.cursor() as cursor:
+            sql = query.insert
+            cursor.execute(sql, (arrdate, airline, fltno, prefix, acreg, ata, atd, bay, chk,
+                                 watersvc, wastesvc, afac, gpu, asu, acu, brk, cherry, platform, int(pkg), int(padd),
+                                 int(sadd), int(add),
+                                 int(zadd), int(cadd), int(madd), int(worked), ovnbay, tpc, waterdrain, wastedrain,
+                                 fueldrain, stand, acwsh,
+                                 mech1, mech2, eng, tda, fltrmk, record))
+            conn.commit()
+        return redirect(url_for('recently'))
+
+
+@app.route('/recently')
+def recently():
+    with conn:
+        cur = conn.cursor()
+        cur.execute(query.recently)
+        recentlyadded = cur.fetchall()
+        return render_template('recently.html', title='Recently Added', data=recentlyadded)
 
 
 if __name__ == "__main__":
--- query.py
+++ query.py
@@ -1,4 +1,4 @@
-flightSql = """SELECT COALESCE(airline, 'TOTAL'),
+totalflt = """SELECT COALESCE(airline, 'TOTAL'),
             COUNT(CASE WHEN MONTH(arrdate) = 1 THEN airline END) AS 'JAN',
             COUNT(CASE WHEN MONTH(arrdate) = 2 THEN airline END) AS 'FEB',
             COUNT(CASE WHEN MONTH(arrdate) = 3 THEN airline END) AS 'MAR',
@@ -12,7 +12,7 @@ flightSql = """SELECT COALESCE(airline, 'TOTAL'),
             COUNT(CASE WHEN MONTH(arrdate) = 11 THEN airline END) AS 'NOV',
             COUNT(CASE WHEN MONTH(arrdate) = 12 THEN airline END) AS 'DEC',
             COUNT(airline) AS 'TOTAL'
-            FROM fltlog 
+            FROM flight 
             WHERE ata IS NOT NULL AND chk != 'RTB' 
             GROUP BY airline
             WITH ROLLUP"""
@@ -30,8 +30,7 @@ totalChk = """SELECT airline, chk,
             COUNT(CASE WHEN MONTH(arrdate) = 10 THEN chk END) AS 'OCT',
             COUNT(CASE WHEN MONTH(arrdate) = 11 THEN chk END) AS 'NOV',
             COUNT(CASE WHEN MONTH(arrdate) = 12 THEN chk END) AS 'DEC'
-            FROM fltlog 
-            WHERE chk != 'RTB' 
+            FROM flight 
             GROUP BY airline, chk"""
 
 totalOvn = """SELECT airline, 
@@ -47,7 +46,7 @@ totalOvn = """SELECT airline,
             COUNT(CASE WHEN MONTH(arrdate) = 10 THEN chk END) AS 'OCT',
             COUNT(CASE WHEN MONTH(arrdate) = 11 THEN chk END) AS 'NOV',
             COUNT(CASE WHEN MONTH(arrdate) = 12 THEN chk END) AS 'DEC'
-            FROM fltlog 
+            FROM flight 
             WHERE ata IS NOT NULL AND atd IS NULL AND chk != 'RTB' 
             GROUP BY airline"""
 
@@ -58,9 +57,7 @@ add = """SELECT DATE_FORMAT(arrdate, '%M'),
     SUM(zadd) AS "ZADD", 
     SUM(cadd) AS "CADD", 
     SUM(madd) AS "MADD", 
-    SUM(worked) AS "WORKED"
-    FROM fltlog
-    GROUP BY MONTH(arrdate)"""
+    FROM fltlog"""
 
 wy = """SELECT COALESCE(airline, 'TOTAL'), 
         COUNT(CASE WHEN MONTH(arrdate) = 1 THEN chk END) AS 'JAN',
@@ -76,27 +73,12 @@ wy = """SELECT COALESCE(airline, 'TOTAL'),
         COUNT(CASE WHEN MONTH(arrdate) = 11 THEN chk END) AS 'NOV',
         COUNT(CASE WHEN MONTH(arrdate) = 12 THEN chk END) AS 'DEC',
         COUNT(chk) AS 'TOTAL'
-        FROM fltlog 
+        FROM flight 
         WHERE chk = 'WY' 
         GROUP BY airline
         WITH ROLLUP"""
 
-pkdadd = """SELECT DATE_FORMAT(arrdate, '%M') AS 'MONTH',
-        SUM(pkg) AS 'PKG',
-        SUM(padd) AS 'PADD',
-        SUM(sadd) AS 'SADD',
-        SUM(`add`) AS 'ADD',
-        SUM(zadd) AS 'ZADD',
-        SUM(cadd) AS 'CADD',
-        SUM(madd) AS 'MADD',
-        SUM(worked) AS 'WORKED',
-        SUM(padd+sadd+`add`+zadd+cadd+madd) AS 'TOTAL ADD CLRD'
-        FROM fltlog
-        WHERE ata IS NOT NULL and atd IS NULL
-        GROUP BY MONTH(arrdate)
-        WITH ROLLUP"""
-
-sumadd = """SELECT SUM(CASE WHEN MONTH(arrdate) = 1 THEN padd+sadd+`add`+zadd+cadd+madd ELSE 0 END) AS '1',
+addclrd = """SELECT SUM(CASE WHEN MONTH(arrdate) = 1 THEN padd+sadd+`add`+zadd+cadd+madd ELSE 0 END) AS '1',
     SUM(CASE WHEN MONTH(arrdate) = 2 THEN padd+sadd+`add`+zadd+cadd+madd ELSE 0 END) AS '2',
     SUM(CASE WHEN MONTH(arrdate) = 3 THEN padd+sadd+`add`+zadd+cadd+madd ELSE 0 END) AS '3',
     SUM(CASE WHEN MONTH(arrdate) = 4 THEN padd+sadd+`add`+zadd+cadd+madd ELSE 0 END) AS '4',
@@ -108,4 +90,47 @@ sumadd = """SELECT SUM(CASE WHEN MONTH(arrdate) = 1 THEN padd+sadd+`add`+zadd+ca
     SUM(CASE WHEN MONTH(arrdate) = 10 THEN padd+sadd+`add`+zadd+cadd+madd ELSE 0 END) AS '10',
     SUM(CASE WHEN MONTH(arrdate) = 11 THEN padd+sadd+`add`+zadd+cadd+madd ELSE 0 END) AS '11',
     SUM(CASE WHEN MONTH(arrdate) = 12 THEN padd+sadd+`add`+zadd+cadd+madd ELSE 0 END) AS '12'
-    FROM fltlog"""
+    FROM flight"""
+
+addworked = """SELECT SUM(CASE WHEN MONTH(arrdate) = 1 THEN worked ELSE 0 END) AS '1',
+    SUM(CASE WHEN MONTH(arrdate) = 2 THEN worked ELSE 0 END) AS '2',
+    SUM(CASE WHEN MONTH(arrdate) = 3 THEN worked ELSE 0 END) AS '3',
+    SUM(CASE WHEN MONTH(arrdate) = 4 THEN worked ELSE 0 END) AS '4',
+    SUM(CASE WHEN MONTH(arrdate) = 5 THEN worked ELSE 0 END) AS '5',
+    SUM(CASE WHEN MONTH(arrdate) = 6 THEN worked ELSE 0 END) AS '6',
+    SUM(CASE WHEN MONTH(arrdate) = 7 THEN worked ELSE 0 END) AS '7',
+    SUM(CASE WHEN MONTH(arrdate) = 8 THEN worked ELSE 0 END) AS '8',
+    SUM(CASE WHEN MONTH(arrdate) = 9 THEN worked ELSE 0 END) AS '9',
+    SUM(CASE WHEN MONTH(arrdate) = 10 THEN worked ELSE 0 END) AS '10',
+    SUM(CASE WHEN MONTH(arrdate) = 11 THEN worked ELSE 0 END) AS '11',
+    SUM(CASE WHEN MONTH(arrdate) = 12 THEN worked ELSE 0 END) AS '12'
+    FROM flight"""
+
+pkg = """SELECT SUM(CASE WHEN MONTH(arrdate) = 1 THEN pkg ELSE 0 END) AS '1',
+    SUM(CASE WHEN MONTH(arrdate) = 2 THEN pkg ELSE 0 END) AS '2',
+    SUM(CASE WHEN MONTH(arrdate) = 3 THEN pkg ELSE 0 END) AS '3',
+    SUM(CASE WHEN MONTH(arrdate) = 4 THEN pkg ELSE 0 END) AS '4',
+    SUM(CASE WHEN MONTH(arrdate) = 5 THEN pkg ELSE 0 END) AS '5',
+    SUM(CASE WHEN MONTH(arrdate) = 6 THEN pkg ELSE 0 END) AS '6',
+    SUM(CASE WHEN MONTH(arrdate) = 7 THEN pkg ELSE 0 END) AS '7',
+    SUM(CASE WHEN MONTH(arrdate) = 8 THEN pkg ELSE 0 END) AS '8',
+    SUM(CASE WHEN MONTH(arrdate) = 9 THEN pkg ELSE 0 END) AS '9',
+    SUM(CASE WHEN MONTH(arrdate) = 10 THEN pkg ELSE 0 END) AS '10',
+    SUM(CASE WHEN MONTH(arrdate) = 11 THEN pkg ELSE 0 END) AS '11',
+    SUM(CASE WHEN MONTH(arrdate) = 12 THEN pkg ELSE 0 END) AS '12'
+    FROM flight"""
+
+staff = "SELECT name FROM staff"
+
+eic = "SELECT name FROM staff WHERE role = 'LAME'"
+
+insert = """INSERT INTO flight (arrdate, airline, fltno, prefix, acreg, ata, atd, bay, chk, 
+    watersvc, wastesvc, afac, gpu, asu, acu, brk, cherry, platform, pkg, padd, sadd, `add`, 
+    zadd, cadd, madd, worked, ovnbay, tpc, waterdrain, wastedrain, fueldrain, stand, wash, 
+    mech1, mech2, eng, tda, fltrmk, record) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
+    %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+
+recently = """SELECT id, arrdate, airline, fltno, prefix, acreg, ata, atd, bay, chk 
+    FROM flight
+    ORDER BY id DESC
+    LIMIT 10"""
